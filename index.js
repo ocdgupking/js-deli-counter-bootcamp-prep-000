7db76a6c70bf9a66de94a336f7867eb2486f3c86
guptaco@ymail.com <guptaco@ymail.com>
2018-03-07 04:11:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function nowServing(katzDeliLine){
 function currentLine(katzDeliLine){
   var listLine = []
   for (var i = 0; i < katzDeliLine.length; i++){
-    listLine.push ([i+1] + '. ' + katzDeliLine[i])
+    listLine.push (' ' + [i+1] + '. ' + katzDeliLine[i])
   }
   if (katzDeliLine.length > 0) {
      return ("The line is currently: " + listLine)
