3623ab44f6c807fe16c796a0428882ec7e7aa7ad
guptaco@ymail.com <guptaco@ymail.com>
2018-03-07 03:06:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name){
-  katzDeli = katzDeli.push(katzDeliLine + name)
+  katzDeli = katzDeliLine.push(katzDel)
   return (katzDeli.length + 1)
 }
