43bfd3a9fd62ddd19e8c9d29b8b77d90d1fcb7d0
guptaco@ymail.com <guptaco@ymail.com>
2018-03-07 03:10:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name){
-  katzDeliLine.push(name)
-  
+  katzDeli = katzDeliLine.push(name)
+  return "Welcome, " + name ".  You are number " (katzDeli.length + 1)
 }
