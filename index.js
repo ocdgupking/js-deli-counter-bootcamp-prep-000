43697442232f2dfd4b83d21c25df901f2777ab05
guptaco@ymail.com <guptaco@ymail.com>
2018-03-07 03:43:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,10 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
+  if (katzDeliLine > 0) {
   return "The line is currently: " + katzDeliLine
+}
+  else {
+    return "The line is currently empty."
+  }
 }
