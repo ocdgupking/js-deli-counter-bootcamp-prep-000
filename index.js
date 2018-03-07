ae98d27aba04526a191f8c5bda934df3e61ff6ee
guptaco@ymail.com <guptaco@ymail.com>
2018-03-07 03:52:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function nowServing(katzDeliLine){
 
 function currentLine(katzDeliLine){
   if (katzDeliLine.length > 0) {
+    for (i = 0; )
   return "The line is currently: " + katzDeliLine. + katzDeliLine
 }
   else {
